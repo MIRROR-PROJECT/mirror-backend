b614cc2d4cd4acf88d93c49c529f674ad2dcaed0
kayunni <gykim0129@gmail.com>
2026-01-04 00:27:48 -0800

    Feat: 일주일치 루틴 조회 api 라우터 등록

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from . import models, database, schemas
-from .api import setup, routines, my, onboarding, auth  
+from .api import setup, routines, my, onboarding, auth, studyroom
 
 # --- 비동기 DB 초기화 함수 ---
 async def init_db():
@@ -43,3 +43,4 @@ app.include_router(routines.router)
 app.include_router(my.router)
 app.include_router(onboarding.router)
 app.include_router(auth.router) 
+app.include_router(studyroom.router) 
